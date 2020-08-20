647e195ec502713d04454bbf4da122e97fd43c10
shreyash <67905454+shreyash@users.noreply.github.com>
2020-08-21 01:19:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,13 +18,4 @@ function topFunction() {
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function slideshow(){
-  for(slide_var=1; slide_var<4; slide_var++){
-    document.getElementById("slideshow_box").style.animation = "fade-out 1000ms"
-    await sleep(1000);
-    document.getElementById("slideshow_box").src = "landing"+slide_var.toString()+".jpg";
-    document.getElementById("slideshow_box").style.animation = "fade-in 1000ms"
-    await sleep(3000)
-  }
-  slideshow()
-}
+
